eeff4c355d19f6fb49abe317a1d5a72796b79489
Indu Pallapothula <indupallapothu91@gmail.com>
2025-11-28 07:25:54 +0000

    int_suppliers and dim_suppliers

--- models/marts/dim_suppliers.sql
+++ models/marts/dim_suppliers.sql
@@ -1,16 +1,24 @@
+{{ config(materialized='view') }}  -- change to 'incremental' if needed
+
 with supps as (
-    selectsupplier_id,
-    supplier_name,
-    account_balance,
-    phone_number,
-    supplier_address,
-    nation as nation_name,
-    region as region_name,
-    updated_timefrom {{ ref('int_suppliers')}}
+    select
+        supplier_id,
+        supplier_name,
+        account_balance,
+        phone_number,
+        supplier_address,
+        nation_name as nation_name,
+        region_name as region_name,
+        updated_time
+    from {{ ref('int_suppliers') }}
+    
     {% if is_incremental() %}
-    where updated_time < (select max(updated_time) from {{this}})
+        where updated_time > (select max(updated_time) from {{ this }})
     {% endif %}
-
-
 )
-select *, {{ dbt_meta() }} from supps
+
+select 
+    *,
+    {{ dbt_meta() }}
+from supps
+
